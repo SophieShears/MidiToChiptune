92bb7b0af7f68a1e2c09ab1c051a6cf2a37c2e83
Sophie <sophieshears01@gmail>
2021-03-17 13:47:00 -0500

    Added ability to stage parts for playback

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 function playSound() {
-    // Get all the note values of a track
-    midiNotes = getMidi();    
-    // Create a synth to play them
-    const synth = new Tone.Synth().toDestination();
+    // Get all the midi data
+    midiData = getMidi();    
 
     // Track the time since button press. SHOULD BE Tone.Transport instead to be more accurate
     const now = Tone.now();
+   
     // If a midi file has been loaded play it
-    midiNotes.then((midiNotes) => {
-        midiNotes.forEach(note => {
-            synth.triggerAttackRelease(note.name, note.duration, now + note.time, note.velocity);
-        });
+    midiData.then((midiData) => {
+        
+        // Establish an intrument for each track
+        const instruments = getInstruments(midiData);
+        const notes = getNotes(midiData);
+
+        // Load and schedule each part for tracks that have notes
+        getParts(notes, instruments);
+
+        // Playback
+        Tone.Transport.start()
+  
     })
 }
--- midi.js
+++ midi.js
@@ -1,19 +1,61 @@
+// Get all midi data from uploaded file
 async function getMidi() {
     const midiFile = document.getElementById('file-selector').files[0];
     const midiURL = window.URL.createObjectURL(midiFile);
     const midi = await Midi.fromUrl(midiURL);
+
+    return midi;
+}
+
+
+// Get every instrument based on their instrument number
+function getInstruments(midiData) {
+    instruments = {};
     
-    //get the tracks
-    const allNotes = [];
-    const track = midi.tracks[1].notes;
-    for (var i=0; i < track.length; i++) {
-        allNotes[i] = {
-            name: track[i].name,
-            duration: track[i].duration,
-            time: track[i].time,
-            velocity: track[i].velocity
-        };
+    for (let track = 0; track < midiData.tracks.length; track++) {
+        instNum = midiData.tracks[track].instrument.number;
+        if (midiData.tracks[track].precussion) {
+            instruments[track] = new Tone.NoiseSynth().toDestination();
+        } else {
+            instruments[track] = new Tone.PolySynth().toDestination();
+        }
     }
 
-    return allNotes;
+    return instruments
+}
+
+
+// Get every note for every track with notes in it
+function getNotes(midiData) {
+    notes = {}
+    
+    // Add notes to track if it has, otherwise leave empty
+    for (let track = 0; track < midiData.tracks.length; track++) {
+        notes[track] = []
+        if (midiData.tracks[track].notes.length > 0) {
+            midiData.tracks[track].notes.forEach(note => {
+                notes[track].push({
+                    time: note.time,
+                    duration: note.duration,
+                    note: note.name,
+                    velocity: note.velocity
+                })
+            })
+        }
+    }
+        
+    return notes
+}
+
+// Creates Tone.js "Part" for every track that has notes, then schedules for playback
+function getParts(notes, instruments) {
+    parts = {}
+
+    for (let track = 0; track < Object.keys(notes).length; track++) {
+        if (notes[track].length) {
+            parts[track] = new Tone.Part(((time, value) => {
+                instruments[track].triggerAttackRelease(value.note, value.duration, time, value.velocity);
+            }), notes[track]).start(0);
+        }
+    } 
 }
