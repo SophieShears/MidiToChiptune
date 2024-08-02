8027018dd12693de5e6b242f846939811c41c15f
rupel <23055682+rupel190@users.noreply.github.com>
2024-08-02 04:26:13 +0200

    save of intermediate state, recording logic is being overhauled

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+
+let recorder;
+let download;
+const errorMessage = document.getElementById('error-message');
+
 function loadFile() {
+    console.log('Tone.js custom version:', Tone.version);
+    
+    // Init recorder
+    initRecorder(); 
     // Stop any playback and clear current song
     stop();
     Tone.Transport.cancel();
@@ -7,11 +16,10 @@ function loadFile() {
     Tone.getDestination().volume.value = document.getElementById('volSlider').value;
     
     // Get all the midi data
-    const midiData = getMidi();    
+    const midiData = getMidi();   
 
     // If a midi file has been  it for playback
     midiData.then((midiData) => {
-
         // Establish an intrument/analyers/notes for each track
         const instruments = getInstruments(midiData);
         getAnalysers(instruments);
@@ -28,6 +36,42 @@ function loadFile() {
             }, time);
         });
     })
+
+}
+
+function initRecorder() {
+    recorder = new Tone.Recorder();
+    console.log('Init recorder: ', recorder);
+    Tone.getDestination().connect(recorder);
+
+    Tone.Transport.on('start', () => {
+        console.log('Start event, current recorder state: ', recorder.state);
+        recorder.start();
+        console.log('After starting: ', recorder.state);
+    });
+
+    Tone.Transport.on('stop', () => {
+        console.log('Stop event, current recorder state: ', recorder.state);
+        
+        setTimeout(async () => {
+            // recording is returned as a blob
+            const recording = await recorder.stop();
+            // download by creating an anchor element and blob url
+            const url = URL.createObjectURL(recording);
+            const anchor = document.createElement("a");
+            anchor.download = "recording.webm";
+            anchor.href = url;
+            // anchor.click();
+            download = anchor;
+        }, 500); // tail end
+        console.log('After stopping: ', recorder.state);
+    });
+
+    Tone.Transport.on('pause', () => {
+        console.log('Pause event, current recorder state: ', recorder.state);
+        recorder.pause();
+        console.log('After pausing: ', recorder.state);
+    })
 }
 
 
@@ -36,19 +80,42 @@ function setVolume(sliderVal) {
     Tone.getDestination().volume.value = sliderVal;
 }
 
-
-// Start playback
 function playPause() {
     if (Tone.Transport.state === 'started') {
+        console.log('Pause tone');
         Tone.Transport.pause();
     }
-    else {
+    else if(Tone.Transport.state === 'paused' || Tone.Transport.state === 'stopped') {
+        console.log('Resume tone');
         Tone.Transport.start()
     }  
+
 }
   
-
-// Stop playback
 function stop() {
-    Tone.Transport.stop();
+    if (Tone.Transport.state !== 'stopped') {
+        console.log('Stop tone');
+        Tone.Transport.stop();
+    }
 }
+
+function save() {
+    let customRecorder = new Tone.Recorder();
+    customRecorder.start();
+    console.log('customrecorder: ', customRecorder.state);
+    customRecorder.pause();
+    console.log('customrecorder: ', customRecorder.state);
+    customRecorder.start();
+    console.log('customrecorder: ', customRecorder.state);
+
+
+    console.log('Download clicked!');
+    if (download) {
+        download.click();
+    } else {
+        errorMessage.textContent = 'No recording available. Load and play first.';
+        errorMessage.style.display = 'block'; // Show error message
+        setTimeout(() => { errorMessage.style.display = 'none'; }, 5000);
+    }
+}
+
